34aa3b55d9e40b638507ca2a596ff23c90e04748
Jeff Hacker <reachjkh@yahoo.com>
2015-09-01 22:08:11 -0400

    removed

--- crud/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url
-
-from api.views import hello
-
-urlpatterns = [
-    url(r'hello/', hello),
-    url(r'movie/', api_movie_list_view, name="api_movie_list_view")
-
-]
